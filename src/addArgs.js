0299663eb574539e4795e501e10bb88f0b7c46eb
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2017-01-20 07:33:39 +0000

    Clearer commenting in src/addArgs.js

--- src/addArgs.js
+++ src/addArgs.js
@@ -3,60 +3,60 @@
  */
 
 /**
- * Takes a function and returns another function which takes an additional first (0'th) argument
- *
- * NB: Does not preserve `this` context.
+ * Returns a wrapper function which calls `fn` and whose signature is the same but for an extra, dummy argument in the
+ * first place. This extra argument is not passed to the wrapped function. NB: Does not preserve `this` context for the
+ * wrapped function.
  *
  * @param {function} fn The function to be wrapped
- * @returns {function} A wrapper for `fn` that takes an extra first (0'th) parameter place, which calls `fn` without it
+ * @returns {function} A wrapper function for `fn` with an extra dummy parameter in the first (`arguments[0]`) place
  */
 function addFirstArg(fn) {
   return (first, ...rest) => fn(...rest);
 }
 
 /**
- * Takes a function and returns another function which takes an additional second (1'th) argument
- *
- * NB: Does not preserve `this` context.
+ * Returns a wrapper function which calls `fn` and whose signature is the same but for an extra, dummy argument in the
+ * second place. This extra argument is not passed to the wrapped function. NB: Does not preserve `this` context for the
+ * wrapped function.
  *
  * @param {function} fn The function to be wrapped
- * @returns {function} A wrapper for `fn` that takes an extra second (1'th) parameter place, which calls `fn` without it
+ * @returns {function} A wrapper function for `fn` with an extra dummy parameter in the second (`arguments[1]`) place
  */
 function addSecondArg(fn) {
   return (first, second, ...rest) => fn(first, ...rest);
 }
 
 /**
- * Takes a function and returns another function which takes an additional third (2'th) argument
- *
- * NB: Does not preserve `this` context.
+ * Returns a wrapper function which calls `fn` and whose signature is the same but for an extra, dummy argument in the
+ * third place. This extra argument is not passed to the wrapped function. NB: Does not preserve `this` context for the
+ * wrapped function.
  *
  * @param {function} fn The function to be wrapped
- * @returns {function} A wrapper for `fn` that takes an extra third (2'th) parameter place, which calls `fn` without it
+ * @returns {function} A wrapper function for `fn` with an extra dummy parameter in the third (`arguments[2]`) place
  */
 function addThirdArg(fn) {
   return (first, second, third, ...rest) => fn(first, second, ...rest);
 }
 
 /**
- * Takes a function and returns another function which takes an additional fourth (3'th) argument
- *
- * NB: Does not preserve `this` context.
+ * Returns a wrapper function which calls `fn` and whose signature is the same but with an extra, dummy argument in the
+ * fourth place. This extra argument is not passed to the wrapped function. NB: Does not preserve `this` context for the
+ * wrapped function.
  *
  * @param {function} fn The function to be wrapped
- * @returns {function} A wrapper for `fn` that takes an extra fourth (3'th) parameter place, which calls `fn` without it
+ * @returns {function} A wrapper function for `fn` with an extra dummy parameter in the fourth (`arguments[3]`) place
  */
 function addFourthArg(fn) {
   return (first, second, third, fourth, ...rest) => fn(first, second, third, ...rest);
 }
 
 /**
- * Takes a function and returns another function which takes an additional fifth (4'th) argument
- *
- * NB: Does not preserve `this` context.
+ * Returns a wrapper function which calls `fn` and whose signature is the same but with an extra, dummy argument in the
+ * fifth place. This extra argument is not passed to the wrapped function. NB: Does not preserve `this` context for the
+ * wrapped function.
  *
  * @param {function} fn The function to be wrapped
- * @returns {function} A wrapper for `fn` that takes an extra fifth (4'th) parameter place, which calls `fn` without it
+ * @returns {function} A wrapper function for `fn` with an extra dummy parameter in the fifth (`arguments[4]`) place
  */
 function addFifthArg(fn) {
   return (first, second, third, fourth, fifth, ...rest) => fn(first, second, third, fourth, ...rest);
